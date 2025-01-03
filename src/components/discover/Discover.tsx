bf02ec682e0ebbb1b4ce1a55af6579cd4e2fe343
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-01-03 16:19:20 +0100

    Enhance Discover component layout and styles; implement rounded shape display with improved responsiveness and scrollbar customization

--- src/components/discover/Discover.tsx
+++ src/components/discover/Discover.tsx
@@ -1,9 +1,13 @@
 import { RoundedShape } from "../../itemDiv/roundedShape/RoundedShape";
+import styles from "./Discover.module.css";
 
 export const Discover = () => {
   return (
-    <div>
-      <RoundedShape />
+    <div className={styles.container}>
+      <h2>Discover gifts for every occasion</h2>
+      <div className={styles.roundedShapeWrapper}>
+        <RoundedShape />
+      </div>
     </div>
   );
 };
--- src/itemDiv/roundedShape/RoundedShape.tsx
+++ src/itemDiv/roundedShape/RoundedShape.tsx
@@ -3,17 +3,28 @@ import styles from "./roundedShape.module.css";
 
 export const RoundedShape = () => {
   const { fetchedData } = useCart();
-  const limitedData = fetchedData?.slice(10, 20);
+  const limitedData = fetchedData?.slice(10, 40); // limiting data to show only products 10 to 20
+  console.log(limitedData);
 
   return (
     <div className={styles.container}>
       {limitedData?.map((item) => {
+        const imageSrc =
+          typeof item.image === "string" ? item.image : "/fallback-image.jpg";
+
         return (
-          <div key={item.id} className={styles.product}>
-            <div>
-              <img src="" alt="" />
+          <div
+            key={item.id}
+            className={styles.product}
+            style={{ color: "black" }}
+          >
+            <div
+              className={styles.roundDiv}
+              style={{ backgroundImage: `url(${imageSrc})` }}
+            >
+              hg
             </div>
-            <p></p>
+            <p>{item.category}</p>
           </div>
         );
       })}
