cdeb7e5f7d6078c86b40bc43c989fac3b25bf864
Pogbayoe <adebayooluwasegun335@gmail.com>
2025-01-03 08:50:56 +0100

    Update Board component layout and styles; adjust main height and add image container

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -2,12 +2,20 @@ import styles from "./board.module.css";
 export const Board = () => {
   return (
     <div className={styles.container}>
-      <img
-        className={styles.img}
-        src="/public/images/ecommerce-3640321.jpg"
-        alt=""
-      />
-      <button className={styles.shopNow}> Shop now</button>
+      <section className={styles.imgContainer}>
+        <img
+          className={styles.imgOne}
+          src="/public/images/ecommerce-3640321.jpg"
+          alt=""
+        />
+        <img
+          className={styles.imgTwo}
+          src="/public/images/online-3964531_1280.jpg"
+          alt=""
+        />
+      </section>
+
+      <button className={styles.shopNow}>Shop now</button>
     </div>
   );
 };
